0721a1da6bd2a6d9592732b7bc60b89370b5be72
Yonis Savary <yonissavary@outlook.fr>
2020-09-05 22:21:51 +0200

    added autoload feature and improved storage data structure

--- src/cascade.js
+++ src/cascade.js
@@ -1,53 +1,100 @@
+/**
+ * CASCADE LIVE SCRIPT
+ * STORAGE STRUCTURE (=>key)
+ * 
+ * => config
+ *      =>  autoload (bool)
+ *          autoload custom css is one exist for the actual domain
+ *      =>  validate (bool)
+ *          is validate checkbox checked by default ?
+ * 
+ * => css
+ *      =>  text
+ *          custom css to load
+ */
+
+
+// Buttons
 let refreshButton   = document.querySelector("#refreshButton");
 let saveButton      = document.querySelector("#saveButton");
-let cssInput        = document.querySelector("#cssCode")
-let validationSpan  = document.querySelector("#validationSpan")
+// Main css text area
+let cssInput        = document.querySelector("#cssCode");
+// Validation input / output
 let validationCheck = document.querySelector("#validationCheck");
+let validationSpan  = document.querySelector("#validationSpan");
 
-function getStorageCss()
+let autoloadCSSCheck = document.querySelector("#autoloadCSSCheck");
+
+function loadConfig()
 {
-    browser.storage.local.get("cascadeLive").then((ok, err)=>{
-        if (err)
+    browser.storage.local.get("validate").then((data, err)=>
+    {
+        if (err) console.log(err);
+        if (data.validate !=  undefined)
         {
-            console.log(err)
+            validationCheck.checked = data.validate;
         }
         else
         {
-            if (ok.cascadeLive)
-            {
-                cssInput.value = ok.cascadeLive.text;
-            }
-            else
-            {
-                cssInput.value = `/* type your custom css rules here to add them to the document */\n:root\n{\n\tfont-size: 16px;\n}`;
-            }
+            browser.storage.local.set({ validate: true });
         }
-    })
-}
-
-function saveStorageCSS(value)
-{
-    let cascadeLive = {
-        text: value
-    }
-    browser.storage.local.set({cascadeLive});
-}
-
-function handleKey(e)
-{
-    if (e.key === "}")
+    });
+    browser.storage.local.get("autoload").then((data, err)=>
     {
-        refresh();
-    }
-    
+        if (err) console.log(err);
+        if (data.autoload !=  undefined)
+        {
+            autoloadCSSCheck.checked = data.autoload;
+        }
+        else
+        {
+            browser.storage.local.set({ autoload: false });
+        }
+    });
 }
 
-function refresh()
+function saveStorageConfig()
+{
+    let config = {
+        validate: validationCheck.checked,
+        autoload: autoloadCSSCheck.checked 
+    }
+    console.log(config)
+    browser.storage.local.set(config)
+}
+
+function loadStorageCSS()
+{
+    browser.storage.local.get("css").then((data, err)=>
+    {
+        if (err) console.log(err);
+        if (data.css != undefined)
+        {
+            cssInput.value = data.css;
+        }
+        else
+        {
+            cssInput.value = `/* place your custom css here */\n:root\n{\n\tfont-size: 16px;\n}`
+        }
+    });
+}
+
+function saveStorageCSS(cssValue)
+{
+    let css = { css: cssValue }
+    browser.storage.local.set(css)
+}
+
+function applyCss()
 {
     const cssRegex = /((?:^\s*)([\w#.@*,:\-.:>,*\s]+)\s*{(?:[\s]*)((?:[A-Za-z\- \s]+[:]\s*['"0-9\w .,\/()\-!%]+;?)*)*\s*}(?:\s*))/gi
+    const commentRegex = /\/\*[^\*]*\*\//gi;
+
     let rules = cssInput.value;
-    const O_rules = rules; // original rules before modifications
-    rules = rules.replace(/\n/gi,"").replace(/\/\*[^\*]*\*\//gi,"")
+    const ORIGINAL_RULES = rules; // original rules before modifications
+
+    rules = rules.replace(/\n/gi,"")
+    rules = rules.replace(commentRegex,"")
     if (rules.match(cssRegex) === null && validationCheck.checked) 
     {
         validationSpan.style.color = "red";
@@ -56,50 +103,40 @@ function refresh()
     else
     {
         browser.tabs.insertCSS({code: rules}).then(()=>{
+            saveStorageCSS(ORIGINAL_RULES)
             validationSpan.style.color = "green";
             validationSpan.innerText = "CSS Injected";
-            saveStorageCSS(O_rules)
         })
     }
 }
 
-// thanks to : https://jsfiddle.net/2wAzx/13/
-function enableTab(id) {
-    var el = document.getElementById(id);
-    el.onkeydown = function(e) {
-        if (e.keyCode === 9) { // tab was pressed
-
-            // get caret position/selection
-            var val = this.value,
-                start = this.selectionStart,
-                end = this.selectionEnd;
-
-            // set textarea value to: text before caret + tab + text after caret
-            this.value = val.substring(0, start) + '\t' + val.substring(end);
-
-            // put caret at right position again
-            this.selectionStart = this.selectionEnd = start + 1;
-
-            // prevent the focus lose
-            return false;
-
-        }
-    };
-}
-// Enable the tab character onkeypress (onkeydown) inside textarea...
-// ... for a textarea that has an `id="my-textarea"`
-enableTab('cssCode');
-
-
-refreshButton.onclick = refresh;
-cssInput.addEventListener("keyup", handleKey)
-saveButton.onclick = ()=>{
-    saveStorageCSS(cssInput.value)
-}
-getStorageCss();
-
 /**
-    "background": {
-        "scripts": ["./src/cascade_debug.js"]
-    }
+ * Allow tabs on cssCode textarea
  */
+document.querySelector("#cssCode").onkeydown = function(e) {
+    if (e.keyCode === 9) { // tab was pressed
+
+        // get caret position/selection
+        var val = this.value,
+            start = this.selectionStart,
+            end = this.selectionEnd;
+
+        // set textarea value to: text before caret + tab + text after caret
+        this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+        // put caret at right position again
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        return false;
+    }
+};
+
+loadConfig();
+loadStorageCSS();
+
+refreshButton.onclick = applyCss;
+saveButton.onclick = ()=>{ saveStorageCSS(cssInput.value); };
+
+autoloadCSSCheck.onclick = saveStorageConfig;
+validationCheck.onclick = saveStorageConfig;
---
+++ src/cascade_background.js
@@ -0,0 +1,19 @@
+browser.storage.local.get("autoload").then((data, err)=>
+{
+    if (err) console.log(err);
+    if (data.autoload == true )
+    {
+        browser.storage.local.get("css").then((data, err)=>
+        {
+            if (err) console.log(err);
+
+            const commentRegex = /\/\*[^\*]*\*\//gi;
+            let rules = data.css;
+            rules = rules.replace(/[\n\t]/gi,"");
+            rules = rules.replace(commentRegex,"");
+            console.log(rules)
+            let sheet = window.document.styleSheets[0];
+            sheet.insertRule(rules, sheet.cssRules.length);
+        })
+    }
+})
